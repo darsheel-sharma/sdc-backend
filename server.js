41ac589c59ecc05dc296d18838bdc124a7931607
darsheel-sharma <darsheelsharma528@gmail.com>
2026-04-07 18:48:37 +0530

    ok

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5000", "http://127.0.0.1:5000"],
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "http://localhost:5000",
+      "http://127.0.0.1:5000",
+    ],
     credentials: true,
   }),
 );
